6736aed2f31a86a2cf4a1600f8442fd9642eb6ad
TonyDMorris <anthonymorris86@googlemail.com>
2019-03-12 12:07:44 +0000

    working on tab colors

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 function changeColor(navLink) {
   let nav = document.getElementById(`${navLink}`);
-  let others = document.getElementsByClassName("nav-link");
-  others.style.backgroundColor = "green";
+
   nav.style.backgroundColor = "blue";
 }
